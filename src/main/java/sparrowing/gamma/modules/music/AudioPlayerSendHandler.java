a33f802881c8a875b791bd85ffa1d6a38da8f0ba
Sparrow <mel.e.grigg@gmail.com>
2017-11-04 03:56:54 -0500

    Started music implementation

---
+++ src/main/java/sparrowing/gamma/modules/music/AudioPlayerSendHandler.java
@@ -0,0 +1,30 @@
+//package sparrowing.gamma.modules.music;
+//
+//import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+//import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
+//
+//import net.dv8tion.jda.core.audio.AudioSendHandler;
+//
+//public class AudioPlayerSendHandler implements AudioSendHandler {
+//	
+//	private final AudioPlayer audioPlayer;
+//	private AudioFrame lastFrame;
+//
+//	public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
+//		this.audioPlayer = audioPlayer;
+//	}
+//
+//	public boolean canProvide() {
+//		lastFrame = audioPlayer.provide();
+//		return lastFrame != null;
+//	}
+//
+//	public byte[] provide20MsAudio() {
+//		return lastFrame.data;
+//	}
+//
+//	public boolean isOpus() {
+//		return true;
+//	}
+//
+//}
---
+++ src/main/java/sparrowing/gamma/modules/music/MusicPlayer.java
@@ -0,0 +1,20 @@
+//package sparrowing.gamma.modules.music;
+//
+//import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+//import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+//import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+//
+//public class MusicPlayer {
+//	
+//	private AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
+//	private AudioPlayer player;
+//	
+//	public MusicPlayer() {
+//		player = playerManager.createPlayer();
+//	}
+//	
+//	public void play() {
+//		player.playTrack(track);
+//	}
+//
+//}
